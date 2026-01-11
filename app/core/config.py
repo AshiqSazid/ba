4bed2c0ec47f505be77525911955694b2dc3f720
Ashiq Sazid <90910973+AshiqSazid@users.noreply.github.com>
2026-01-12 01:25:00 +0600

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -37,6 +37,7 @@ class Settings(BaseSettings):
 
     # CORS
     BACKEND_CORS_ORIGINS: List[str] = [
+        "https://f-1-u01e.onrender.com",
         "http://localhost:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3000",
