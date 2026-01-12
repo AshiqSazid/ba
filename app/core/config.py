afc0acbcd597fbad78ca3155984a103a9faae132
AshiqSazid <ashiqsazid494@gmail.com>
2026-01-12 12:24:02 +0600

    fix

--- app/core/config.py
+++ app/core/config.py
@@ -37,11 +37,7 @@ class Settings(BaseSettings):
 
     # CORS
     BACKEND_CORS_ORIGINS: List[str] = [
-<<<<<<< HEAD
-        "https://backend-thera.onrender.com",
-=======
         "https://f-1-u01e.onrender.com",
->>>>>>> 0e2ee54 (fix)
         "http://localhost:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3000",
